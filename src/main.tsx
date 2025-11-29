0512cf37ef29e46e67fbc198dc8f27495dd0e941
Jae-Won Chung <jwnchung@umich.edu>
2025-11-29 15:14:31 -0500

    Add Google tags

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (import.meta.env.PROD) {
+  const script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-8NM6L1X6ML';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(...args: unknown[]) { window.dataLayer.push(args); }
+  gtag('js', new Date());
+  gtag('config', 'G-8NM6L1X6ML');
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
