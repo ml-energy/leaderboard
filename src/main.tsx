6b86452def1471c20e8cdb95b8c272d6aaffabb1
Jae-Won Chung <jwnchung@umich.edu>
2025-12-11 17:03:15 -0800

    Fix gtag

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-if (import.meta.env.PROD) {
-  const script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-8NM6L1X6ML';
-  document.head.appendChild(script);
-
-  window.dataLayer = window.dataLayer || [];
-  window.gtag = function(...args: unknown[]) { window.dataLayer.push(args); };
-  window.gtag('js', new Date());
-  window.gtag('config', 'G-8NM6L1X6ML');
-}
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,10 +2,6 @@
 
 declare global {
   const __LAST_UPDATED__: string;
-  interface Window {
-    dataLayer: unknown[];
-    gtag: (...args: unknown[]) => void;
-  }
 }
 
 export {};
